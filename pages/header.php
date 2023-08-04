7edc9ad2707a9ef376b5b2cff6e3bf3a23e17bb0
Milica Mitrovic <milicamitrovicfon@gmail.com>
2023-08-04 17:01:12 +0200

    Kreiran header - header.php

---
+++ pages/header.php
@@ -0,0 +1,20 @@
+<div class='navbar_div'>
+  <nav class="navbar navbar-light sticky-top bottom_sep" style="background-color: #f0e68c;">
+  <a href="index.php"><img src="../img/logo1.png" class="logo-pic"/></a>
+    <a class="navbar-brand a_header" href="#"><p class="text-white">Dance studio "La Fiesta"</p></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" 
+      aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="index.php">Vrste plesova</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="termini.php">Termini</a>
+        </li>
+        </ul>
+    </div>
+  </nav>
+</div>
